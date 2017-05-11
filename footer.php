cc7b13c5e8bf2ca2d749910a141241273bed8baf
n0vad3v <n0vad3v@riseup.net>
2017-05-11 11:20:27 +0800

    Added footer img

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
 <div class="footer" style="text-align: center;">
-    Powered by <a href="https://lunaluna.org">Lizihu Utopia LUNA Association</a>
+    Powered by <a href="https://lunaluna.org"><img src="./img/footer.png" height="50"></a>
     GitHub:<a href="https://github.com/LUNAD3v/areaload">AreaLoad</a>
 </div>
