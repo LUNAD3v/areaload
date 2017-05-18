6324bc458e0cc86f6f2ea7b9d1c635f86b1a3862
n0vad3v <n0vad3v@riseup.net>
2017-05-18 23:25:44 +0800

    Nav-Bar module design

--- c.php
+++ c.php
@@ -28,23 +28,7 @@
 
 <body>
 
-  <!-- Static navbar -->
-  <nav class="navbar navbar-default" style="opacity: 0.8">
-      <div class="container">
-          <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-          </div>
-          <a class="navbar-brand">AreaLoad</a>
-          <div id="navbar" class="navbar-collapse collapse navbar-right">
-              <ul class="nav navbar-nav">
-                <li><a href="./index.php">首页</a></li>
-                <li><a href="./problem.php">申请重新上传</a></li>
-                <li><a href="./uploaded.php">已经上传的学生列表</a></li>
-              </ul>
-          </div>
-          <!--/.nav-collapse -->
-      </div>
-  </nav>
+<?php include('nav.php') ?>
 
   <div class="container">
 
