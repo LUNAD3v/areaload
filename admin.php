4d06b4d4c1e292243dd71e3ae7ca98e7d6687e20
allenliu123 <973400732@qq.com>
2017-06-16 19:31:46 +0800

    add admin.php animation

--- admin.php
+++ admin.php
@@ -31,171 +31,244 @@ $trustlists = $connect->query("SELECT * FROM trustlist");
 
     <!-- Material Design Bootstrap -->
     <link href="./css/mdb.min.css" rel="stylesheet">
+    <style type="text/css">
+        .leftside>li {
+            background: rgba(84,215,149,0.5);
+            height: 50px;
+            width: 100%;
+            line-height: 50px;
+            border-bottom:0.5px solid rgba(84,215,149,0.8); 
+
+        }
+        .leftside>li:hover {
+            background: rgba(84,215,149,0.8);
+        }
+        #second,#third,#fourth,#fifth,#sixth {
+            display: none;
+        }
+        .head{
+            background: rgb(84,215,149);
+            height: 50px;
+            width: 100%;
+            line-height: 50px;
+            font-size: 1.2em;
+            border-bottom:0.5px solid rgba(84,215,149,0.8); 
+        }
+        .bu {
+            border: 0px;
+            float: left;
+            margin-left: 5px;
+        }
+        .li {
+            height: 80px;
+
+        }
+        .glyphicon {
+            float: right;
+            color: green;
+            line-height: 50px;
+        }
+    </style>
 </head>
-
 <body>
-<img src="./img/admin.jpeg" style="position: fixed;width: 100%; opacity: 0.2; z-index: -100" alt="">
-<?php include('./partial/nav.php') ?>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-sm-12">
-			  <div class="panel panel-success">
-			    <div class="panel-heading">课程管理</div>
-			    <div class="panel-body">
-              <ul class="list-group">
-
-              <?php
-              foreach ($array as $course) {
-				$eachcoursename = $course['name'];
-                $eachcourseid = $course['id'];
-				$eachcoursecategory = $course['category'];
-				$eachcoursecategoryid = $course['categoryid'];
-				$counts = $connect->query("SELECT COUNT(*) FROM uploaded WHERE course = '$eachcourseid';");
-                $count = $counts->fetchColumn();
-                echo "<li class=\"list-group-item\">";
-                echo "<span class=\"badge\">". $count . "</span>";
-
-								echo "<b>" . $eachcoursename . "</b>" . " | " . $eachcourseid . " | " . $eachcoursecategory . " | " . "$eachcoursecategoryid";
-								echo "<form action=\"course.php\" method=\"post\">";
-								echo "<input type=\"hidden\" name=\"rstcourseid\" value=\"$eachcourseid\"></input>";
-								echo "<input type=\"hidden\" name=\"rstcoursecategoryid\" value=\"$eachcoursecategoryid\"></input>";
-		    					echo "<button class=\"badge\" input type=\"submit\">重置</button>";
-								echo "</form>";
-								echo "<form action=\"edit.php\" method=\"post\">";
-								echo "<input type=\"hidden\" name=\"editcourseid\" value=\"$eachcourseid\"></input>";
-		    					echo "<button class=\"badge\" input type=\"submit\">修改</button>";
-								echo "</form>";
-								echo "<form action=\"course.php\" method=\"post\">";
-								echo "<input type=\"hidden\" name=\"delcourseid\" value=\"$eachcourseid\"></input>";
-								echo "<button class=\"badge\" input type=\"submit\">删除</button>";
-								echo "</form>";
-
-                echo "</li>";
-              }
-                ?>
-
+    <img src="./img/admin.jpeg" style="position: fixed;width: 100%; opacity: 0.2; z-index: -100" alt="">
+    <?php include('./partial/nav.php') ?>
+    <div class="container">
+        <div class="col-md-3">
+            <ul class="leftside">
+                <li id="li1">&nbsp&nbsp课程管理<span class="glyphicon glyphicon-chevron-right"></span></li>
+                <li id="li2">&nbsp&nbsp分类管理<span class="glyphicon glyphicon-chevron-right"></span></li>
+                <li id="li3">&nbsp&nbsp添加课程<span class="glyphicon glyphicon-chevron-right"></span></li>
+                <li id="li4">&nbsp&nbspTicket<span class="glyphicon glyphicon-chevron-right"></span></li>
+                <li id="li5">&nbsp&nbsp信任学号（段）<span class="glyphicon glyphicon-chevron-right"></span></li>
             </ul>
-		</div>
-	</div>
-
-			<div class="panel panel-success">
-              <div class="panel-heading">分类管理</div>
-              <div class="panel-body">
-				  <?php
-				  $categoryarray = $connect->query("SELECT DISTINCT category,categoryid FROM course;");
-				  foreach ($categoryarray as $category) {
-					$eachcoursecategory = $category['category'];
-					$eachcoursecategoryid = $category['categoryid'];
-	                echo "<li class=\"list-group-item\">";
-
-									echo "<b>" . $eachcoursecategory . "</b>" . " | " . "$eachcoursecategoryid";
-									echo "<form action=\"course.php\" method=\"post\">";
-									echo "<input type=\"hidden\" name=\"delcategoryid\" value=\"$eachcoursecategoryid\"></input>";
-									echo "<button class=\"badge\" input type=\"submit\">删除</button>";
-									echo "</form>";
-
-	                echo "</li>";
-	              }
-				  ?>
-              </div>
-            </div>
-
+        </div>
+        <div class="col-md-9">
             <div class="panel panel-success">
-              <div class="panel-heading">课程管理</div>
-              <div class="panel-body">
-			  <h2>添加课程</h2>
-              <form action="course.php" method="post">
-              <input type="text" name="addcoursename" class="form-control" placeholder="课程名称（例：Web技术基础）" required>
-              <input type="text" name="addcourseid" class="form-control" placeholder="课程ID（例：web）" required>
-			  <input type="text" name="addcategory" class="form-control" placeholder="分类（例：前端）" required>
-			  <input type="text" name="addcategoryid" class="form-control" placeholder="分类ID（例：frontend）" required>
-              <textarea input type="text" class="form-control" rows="2" name="addcourseinfo" placeholder="课程介绍" required></textarea>
-              <textarea input type="text" class="form-control" rows="5" name="addcoursedemand" placeholder="课程要求" required></textarea>
-              <button class="btn btn-lg btn-success btn-block" input type="submit">添加</button>
-              </form>
-              </div>
+                <div id="first">
+                    <div class="head">&nbsp&nbsp课程管理</div>
+                    <div class="panel-body">
+                    <ul class="list-group">
+                    <?php
+                    foreach ($array as $course) {
+                      $eachcoursename = $course['name'];
+                      $eachcourseid = $course['id'];
+                      $eachcoursecategory = $course['category'];
+                      $eachcoursecategoryid = $course['categoryid'];
+                      $counts = $connect->query("SELECT COUNT(*) FROM uploaded WHERE course = '$eachcourseid';");
+                      $count = $counts->fetchColumn();
+                      echo "<li class=\"list-group-item li\">";
+                      echo "<span class=\"badge\">". $count . "</span>";
+
+                          echo "<b>" . $eachcoursename . "</b>" . " | " . $eachcourseid . " | " . $eachcoursecategory . " | " . "$eachcoursecategoryid";
+                          echo "<form action=\"course.php\" method=\"post\">";
+                          echo "<ul><li><input type=\"hidden\" name=\"rstcourseid\" value=\"$eachcourseid\"></input>";
+                          echo "<input type=\"hidden\" name=\"rstcoursecategoryid\" value=\"$eachcoursecategoryid\"></input>";
+                          echo "<button class=\"bu\" input type=\"submit\">重置</button></li>";
+                          echo "</form>";
+                          echo "<li><form action=\"edit.php\" method=\"post\">";
+                          echo "<input type=\"hidden\" name=\"editcourseid\" value=\"$eachcourseid\"></input>";
+                          echo "<button class=\"bu\" input type=\"submit\">修改</button></li>";
+                          echo "</form>";
+                          echo "<li><form action=\"course.php\" method=\"post\">";
+                          echo "<input type=\"hidden\" name=\"delcourseid\" value=\"$eachcourseid\"></input>";
+                          echo "<button class=\"bu\" name=\"delcourseid\" value=\"$eachcourseid\" input type=\"submit\">删除</button>";
+                          echo "</form></li>";
+                          echo "<li><form action=\"course.php\" method=\"post\">";
+                          echo "<input type=\"hidden\" name=\"dlcourseid\" value=\"$eachcourseid\"></input>";
+                          echo "<button class=\"bu\" input type=\"submit\">收作业</button>";
+                          echo "</form></li></ul>";
+
+                      echo "</li>";
+                    }
+                      ?>
+
+                    </ul>
+                    </div>
+                </div>
+                <div id="second">
+                    <div class="head">&nbsp&nbsp分类管理</div>
+                    <div class="panel-body">
+                        <?php
+                        $categoryarray = $connect->query("SELECT DISTINCT category,categoryid FROM course;");
+                        foreach ($categoryarray as $category) {
+                          $eachcoursecategory = $category['category'];
+                          $eachcoursecategoryid = $category['categoryid'];
+                          echo "<li class=\"list-group-item\">";
+
+                                          echo "<b>" . $eachcoursecategory . "</b>" . " | " . "$eachcoursecategoryid";
+                                          echo "<form action=\"course.php\" method=\"post\">";
+                                          echo "<input type=\"hidden\" name=\"delcategoryid\" value=\"$eachcoursecategoryid\"></input>";
+                                          echo "<button class=\"bu\" input type=\"submit\">删除</button>";
+                                          echo "</form>";
+
+                          echo "</li>";
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div id="third">
+                    <div class="head">&nbsp&nbsp添加课程</div>
+                    <div class="panel-body">
+                        <h3>添加课程</h3>
+                        <form action="course.php" method="post">
+                            <input type="text" name="addcoursename" class="form-control" placeholder="课程名称（例：Web技术基础）" required>
+                            <input type="text" name="addcourseid" class="form-control" placeholder="课程ID（例：web）" required>
+                            <input type="text" name="addcategory" class="form-control" placeholder="分类（例：前端）" required>
+                            <input type="text" name="addcategoryid" class="form-control" placeholder="分类ID（例：frontend）" required>
+                            <textarea input type="text" class="form-control" rows="2" name="addcourseinfo" placeholder="课程介绍" required></textarea>
+                            <textarea input type="text" class="form-control" rows="5" name="addcoursedemand" placeholder="课程要求" required></textarea>
+                            <button class="btn btn-lg btn-success btn-block" input type="submit">添加</button>
+                        </form>
+                    </div>
+                </div>
+                <div id="fourth">
+                    <div class="head">&nbsp&nbspTickets</div>
+                    <div class="panel-body">
+                    <ul class="list-group">
+                    <?php
+                    foreach ($tickets as $ticket) {
+                    echo "<li class=\"list-group-item\">";
+                    echo "<b>".$ticket['number']."</b>" . " | ";
+                    echo $ticket['content'];
+                                    echo " | ";
+                                    echo $ticket['phone'];
+                                    echo "</li>";
+                        }
+                    ?>
+                    </ul>
+
+                    <form action="course.php" method="post">
+                    <input type="text" name="delticket" class="form-control" placeholder="学号" required>
+                    <button class="btn btn-lg btn-danger btn-block" input type="submit">删除</button>
+                    </form>
+
+                    </div>
+                </div>
+                <div id="fifth">
+                    <div class="head">&nbsp&nbsp信任学号（段）</div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                        <?php
+                        foreach ($trustlists as $trustlist) {
+                            echo "<li class=\"list-group-item\">";
+                            echo $trustlist['number'];
+                            echo "</li>";
+                        }
+                        ?>
+                        </ul>
+                    </div>
+                    <div class="panel-body">
+                        <h3>添加信任学号（段）</h3>
+                        <p>通配部分使用**代替，如允许所有16级信息学院计算机类的学生上传，则填写：“63160704****”</p>
+                        <form action="course.php" method="post">
+                            <input type="text" name="addtrustlist" class="form-control" placeholder="学号或学号段（例：6316070404**）" required>
+                            <button class="btn btn-lg btn-success btn-block" input type="submit">添加</button>
+                        </form>
+                        <hr>
+                        <h3>删除信任学号（段）</h3>
+                        <form action="course.php" method="post">
+                            <input type="text" name="deltrustlist" class="form-control" placeholder="学号或学号段" required>
+                        <button class="btn btn-lg btn-danger btn-block" input type="submit">删除</button>
+                        </form>
+
+                  </div>
+                </div>
+                </div>
             </div>
+    </div>
 
-          </div><!--<div class="col-md-6 col-sm-12">-->
+    <?php include('./partial/footer.php') ?>
+<!-- JQuery -->
+<script type="text/javascript" src="./js/jquery-3.1.1.min.js"></script>
 
-          <div class="col-md-6 col-sm-12">
-			  <div class="panel panel-danger">
-			    <div class="panel-heading">Tickets</div>
-			    <div class="panel-body">
-              <ul class="list-group">
-              <?php
-              foreach ($tickets as $ticket) {
-                echo "<li class=\"list-group-item\">";
-                echo "<b>".$ticket['number']."</b>" . " | ";
-                echo $ticket['content'];
-								echo " | ";
-								echo $ticket['phone'];
-								echo "</li>";
-              }
-                ?>
-            </ul>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="./js/tether.min.js"></script>
 
-			  <form action="course.php" method="post">
-              <input type="text" name="delticket" class="form-control" placeholder="学号" required>
-              <button class="btn btn-lg btn-danger btn-block" input type="submit">删除</button>
-              </form>
-		  </div>
-	  </div>
-	  <div class="panel panel-danger">
-		<div class="panel-heading">信任学号（段）</div>
-		<div class="panel-body">
-              <ul class="list-group">
-              <?php
-              foreach ($trustlists as $trustlist) {
-                echo "<li class=\"list-group-item\">";
-                echo $trustlist['number'];
-                echo "</li>";
-              }
-                ?>
-            </ul>
-		</div>
-	</div>
-		        <div class="panel panel-danger">
-              <div class="panel-heading">信任学号（段）管理</div>
-              <div class="panel-body">
-			  			<h2>添加信任学号（段）</h2>
-			  			<p>通配部分使用**代替，如允许所有16级信息学院计算机类的学生上传，则填写：“63160704****”</p>
-              <form action="course.php" method="post">
-              <input type="text" name="addtrustlist" class="form-control" placeholder="学号或学号段（例：6316070404**）" required>
-              <button class="btn btn-lg btn-success btn-block" input type="submit">添加</button>
-              </form>
-			  		<hr>
-			  			<h2>删除信任学号（段）</h2>
-			  			<form action="course.php" method="post">
-              <input type="text" name="deltrustlist" class="form-control" placeholder="学号或学号段" required>
-              <button class="btn btn-lg btn-danger btn-block" input type="submit">删除</button>
-              </form>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="./js/bootstrap.min.js"></script>
 
-              </div>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="./js/mdb.min.js"></script>
 
-          </div><!--<div class="panel panel-success">-->
-        </div><!--<div class="col-md-6 col-sm-12">-->
-
-        </div><!--/row-->
-
-      </div> <!-- /container -->
-      <?php include('./partial/footer.php') ?>
-    <!-- SCRIPTS -->
-
-    <!-- JQuery -->
-    <script type="text/javascript" src="./js/jquery-3.1.1.min.js"></script>
-
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="./js/tether.min.js"></script>
-
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
-
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="./js/mdb.min.js"></script>
-
-
-
+<script>
+$(document).ready(function(){
+    $("#li1").click(function(){
+        $("#first").show();
+        $("#second").hide();
+        $("#third").hide();
+        $("#fourth").hide();
+        $("#fifth").hide();
+    });
+    $("#li2").click(function(){
+        $("#first").hide();
+        $("#second").show();
+        $("#third").hide();
+        $("#fourth").hide();
+        $("#fifth").hide();
+     });
+    $("#li3").click(function(){
+        $("#first").hide();
+        $("#second").hide();
+        $("#third").show();
+        $("#fourth").hide();
+        $("#fifth").hide();
+    });
+    $("#li4").click(function(){
+        $("#first").hide();
+        $("#second").hide();
+        $("#third").hide();
+        $("#fourth").show();
+        $("#fifth").hide();
+    });
+    $("#li5").click(function(){
+        $("#first").hide();
+        $("#second").hide();
+        $("#third").hide();
+        $("#fourth").hide();
+        $("#fifth").show();
+    });
+});
+</script>
 </body>
-
 </html>
