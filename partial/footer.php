d515f10095f6e70c68ffe0550b88a0fbd7e6da55
n0vad3v <n0vad3v@riseup.net>
2017-09-07 10:28:51 +0800

    Add image source

--- partial/footer.php
+++ partial/footer.php
@@ -3,4 +3,5 @@
 <div class="footer" style="text-align: center;">
     Powered by <a href="https://lunaluna.org"><img src="./img/footer.png" height="50"></a>
     GitHub:<a href="https://github.com/LUNAD3v/areaload">AreaLoad</a>
+    Image:<a href="https://500px.com/n0vad3v">n0vad3v</a>
 </div>
