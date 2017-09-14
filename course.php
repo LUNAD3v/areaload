232636cf13a350f9d05a32ffc9d038a91a92e158
n0vad3v <n0vad3v@riseup.net>
2017-09-14 15:46:59 +0800

    Randomize download link

--- course.php
+++ course.php
@@ -77,6 +77,38 @@ function Zip($source, $destination)
     return $zip->close();
 }
 
+function random_str($type = 'alphanum', $length = 8)
+{
+    switch($type)
+    {
+        case 'basic'    : return mt_rand();
+            break;
+        case 'alpha'    :
+        case 'alphanum' :
+        case 'num'      :
+        case 'nozero'   :
+                $seedings             = array();
+                $seedings['alpha']    = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                $seedings['alphanum'] = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                $seedings['num']      = '0123456789';
+                $seedings['nozero']   = '123456789';
+
+                $pool = $seedings[$type];
+
+                $str = '';
+                for ($i=0; $i < $length; $i++)
+                {
+                    $str .= substr($pool, mt_rand(0, strlen($pool) -1), 1);
+                }
+                return $str;
+            break;
+        case 'unique'   :
+        case 'md5'      :
+                    return md5(uniqid(mt_rand()));
+            break;
+    }
+}
+
 $connect = new PDO('sqlite:./db/db.sqlite');
 $addcoursename = $_POST['addcoursename'];
 $addcourseid = $_POST['addcourseid'];
@@ -189,8 +221,11 @@ if($dlcourseid)
 	$zipurl = "./upload/" . $dlcategoryid . "/" . $dlcourseid;
 	Zip($zipurl,"./upload/" . $dlcourseid . '.zip');
 	$dlurl = "./upload/" . $dlcourseid . '.zip';
+	$randfilename = random_str('alphanum',10);
+	$tmpdlurl = "./tmp/" . $randfilename . '.zip';
+	copy($dlurl,$tmpdlurl);
 
-	header("Location: $dlurl");
+	header("Location: $tmpdlurl");
 }
 //End Download course
 
