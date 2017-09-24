c9ad73ea082ba2b657d1c14e6a89fd12c8e7638f
allenliu123 <973400732@qq.com>
2017-09-24 21:05:49 +0800

    problem.php can post courseid

--- problem.php
+++ problem.php
@@ -1,5 +1,9 @@
 <?php
   session_start();
+
+  $connect = new PDO('sqlite:./db/db.sqlite');
+  $array = $connect->query("SELECT * FROM course;");
+
  ?>
 <!DOCTYPE html>
 <html lang="zh">
@@ -20,8 +24,28 @@
 
     <!-- Material Design Bootstrap -->
     <link href="./css/mdb.min.css" rel="stylesheet">
-
-
+    <style media="screen">
+    select {
+        width: 100%;
+        height: 30px;
+        background: none;
+        border: none;
+        font-size: 1.2em;
+        margin-top: 7px;
+        appearance:none;/*将默认的select选择框样式清除*/
+        -moz-appearance:none;
+        -webkit-appearance:none;
+        -ms-appearance:none;
+        border-bottom: 0.5px solid rgba(200,200,200,0.8);
+    }
+    select optgroup{
+        background: #B3A5B4;
+    }
+    select option {
+        padding: 2px;
+        padding-left: 20px;
+    }
+    </style>
 </head>
 
 <body>
@@ -38,10 +62,28 @@
 
         <form action="request.php" method="post">
               <div class="row">
-                <div class="col-md-6 col-xs-6">
+                <div class="col-md-4 col-xs-4">
+                <select name="courseid" required>
+                    <option hidden>▼&nbsp&nbsp点击选择课程</option>
+                    <?php
+                    $titlearray = $connect->query("SELECT DISTINCT category,categoryid FROM course;");
+                    foreach ($titlearray as $title)
+                    {
+                        echo "<optgroup label=\"".$title['category']."\" class=\"choice\">";
+                        $tmpcategoryid = $title['categoryid'];
+                        $coursearray = $connect->query("SELECT * FROM course WHERE categoryid = '$tmpcategoryid';");
+                        foreach($coursearray as $row){
+                            echo "<option value=\"".$row['id']."\">".$row['name']."</option>";
+                        }
+                        echo "</optgroup>";
+                    }
+                    ?>
+                </select>
+                </div>
+                <div class="col-md-4 col-xs-4">
                   <input type="text" name="number" placeholder="学号" required>
                 </div><!-- col-md-6 col-xs-6-->
-                <div class="col-md-6 col-xs-6">
+                <div class="col-md-4 col-xs-4">
                   <input type="text" name="phone" placeholder="手机号码" required>
                 </div><!-- col-md-6 col-xs-6-->
               </div><!--row -->
