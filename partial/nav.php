470ab0afb8dee430a000139375e3008bbc312d86
n0vad3v <n0vad3v@riseup.net>
2017-10-11 13:10:59 +0800

    Fix Nav

--- partial/nav.php
+++ partial/nav.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-        </div>
         <a class="navbar-brand"><b>AreaLoad</b></a>
+        </div>
         <div id="navbar" class="navbar-collapse collapse navbar-right">
             <ul class="nav navbar-nav">
             <?php
